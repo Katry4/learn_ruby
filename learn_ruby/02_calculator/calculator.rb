6e965ef1c1812b7a3b8cc3173c5153fb1b418e45
Sergiy Katrych <kratos666666@gmail.com>
2013-01-28 20:17:14 +0200

    02 - added mul,pow and factorial func's.../n I'm awesome..:)

--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -10,6 +10,30 @@ def sum (a)
 return a.inject(0) { |result, elem| result = result + elem}
 end
 
-def multiply (a,b)
-return a*b
+def multiply (a)
+return a.inject(1) { |result, elem| result = result * elem}
+end
+
+def power (a,b)
+i=1;
+ret=a;
+while i<b 
+ret=ret*a
+b=+1
+end
+return ret
+end
+
+def factorial (a)
+fac=0
+b=1
+if a>0
+fac=1
+
+while b<=a
+fac=fac*b
+b=b+1
+end
+end
+return fac
 end
