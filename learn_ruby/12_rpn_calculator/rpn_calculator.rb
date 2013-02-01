ac74bac44f471a08d4547800557eb46e65075029
Sergiy Katrych <kratos666666@gmail.com>
2013-02-02 00:41:07 +0200

    12 - Polish has been defeated

--- learn_ruby/12_rpn_calculator/rpn_calculator.rb
+++ learn_ruby/12_rpn_calculator/rpn_calculator.rb
@@ -40,9 +40,11 @@ class RPNCalculator
 
 attr_accessor :p_stack
 attr_accessor :value
+attr_accessor :oper_arr
 
 def initialize
 @p_stack=[]
+@oper_arr=["+","-","*","/"]
 end
 
 def push(s)
@@ -51,28 +53,108 @@ end
 
 def plus
 
-  
+ if @p_stack.size>=2 then 
 @value=@p_stack.pop + @p_stack.pop
 @p_stack << @value
+else
+	raise "calculator is empty"
+end
 end
 
 def minus
+	 if @p_stack.size>=2 then 
 @value=@p_stack.pop - @p_stack.pop
 @p_stack << @value
+else
+	raise "calculator is empty"
+end
 end
 
 def divide
+if @p_stack.size>=2 then 
 @value=@p_stack.pop.to_f / @p_stack.pop
 @p_stack << @value
+else
+	raise "calculator is empty"
+end
 end
 
 def times
+if @p_stack.size>=2 then 
 @value=@p_stack.pop * @p_stack.pop
 @p_stack << @value
+else
+	raise "calculator is empty"
+end
 end
 
-def value
-  @value
+def tokens (str)
+tmp=str.split(" ")
+tmp.each do 
+	|el	|
+	@oper_arr.include?(el) ? @p_stack << el.to_sym : @p_stack << el.to_i
+end
+@p_stack
+end
+
+
+#### Перепутав завдання  і подумав, що потрібно записати польський запис в нормальній формі з дужками
+def evaluates(str)
+	tmp=str.split(" ")
+	prev_op=2
+
+	tmp.each do
+		| el|
+		if @oper_arr.include?(el) then
+			if el=="-" or el=="+" then cur_op=1 else cur_op=2 end
+
+			if cur_op<prev_op then
+				tmp1=@p_stack.pop
+				tmp2=@p_stack.pop
+				tmp3="(#{tmp1})#{el}#{tmp2}"
+			else
+				tmp1=@p_stack.pop
+				tmp2=@p_stack.pop
+				tmp3="#{tmp1}#{el}#{tmp2}"
+			end
+			@p_stack << tmp
+			
+		else
+			@p_stack << el.to_i
+		end
+	end
+@p_stack
+end
+
+
+def evaluate(str)
+	tmp=str.split(" ")
+	
+	tmp.each do
+		| el |
+		if @oper_arr.include?(el) then
+
+			tmp1=@p_stack.pop
+			tmp2=@p_stack.pop
+			
+			case el
+				when "+"
+					tmp3=tmp1+tmp2
+				when "-"
+					tmp3=tmp1-tmp2
+				when "*"
+					tmp3=tmp1*tmp2
+				when "/"
+					tmp3=tmp1/tmp2
+			end
+			
+			@p_stack << tmp3.to_f
+			
+		else
+			@p_stack << el.to_f
+		end
+	end
+@p_stack.pop
 end
 
 end
